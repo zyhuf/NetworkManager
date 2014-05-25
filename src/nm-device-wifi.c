zyhuf/NetworkManager

commit da59b6d2229b85afbb9e14846931376340dc6030

core: respect connection user permissions for activation/deactivation
This appears to be a bug since the original 0.9.0 release when
connection permissions were implemented.

If all the following are true:
- the user is local (as determined by systemd or ConsoleKit)
- the user has been given the NETWORK_CONTROL PolicyKit permission
- the user is not listed in the connection's permissions
- the user knows the D-Bus object path of a connection which they
have no permissions for

then that user may activate/deactivate connections that are not
visible to that user as determined by the connection permissions.
Fix that by ensuring that these operations check whether the user
has permission.

These operations are *not* affected, and have always checked user
permissions before allowing the operation:
- modifying any connection details
- requesting any secrets or passwords for the connection
- deleting the connection

Backport-of: eb8bc5396e0f41b9ebcba5e45916de8b523168c3

src/nm-manager.c
@@ -222,6 +222,7 @@ typedef struct {
 	guint          dbus_connection_changed_id;
 	NMUdevManager *udev_mgr;
 	NMBluezManager *bluez_mgr;
+	NMSessionMonitor *session_monitor;
 
 	/* List of NMDeviceFactoryFunc pointers sorted in priority order */
 	GSList *factories;
@@ -941,15 +942,17 @@ static void
 pending_activation_check_authorized (PendingActivation *pending,
                                      NMDBusManager *dbus_mgr)
 {
+	NMManagerPrivate *priv;
 	char *error_desc = NULL;
 	gulong sender_uid = G_MAXULONG;
 	GError *error;
 	const char *wifi_permission = NULL;
 	NMConnection *connection;
-	NMSettings *settings;
 
 	g_return_if_fail (pending != NULL);
 	g_return_if_fail (dbus_mgr != NULL);
+
+	priv = NM_MANAGER_GET_PRIVATE (pending->manager);
 
 	if (!nm_auth_get_caller_uid (pending->context, 
 	                             dbus_mgr,
@@ -974,10 +977,8 @@ pending_activation_check_authorized (PendingActivation *pending,
 	 * or an existing connection (for Activate).
 	 */
 	connection = pending->connection;
-	if (!connection) {
-		settings = NM_MANAGER_GET_PRIVATE (pending->manager)->settings;
-		connection = (NMConnection *) nm_settings_get_connection_by_path (settings, pending->connection_path);
-	}
+	if (!connection)
+		connection = (NMConnection *) nm_settings_get_connection_by_path (priv->settings, pending->connection_path);
 
 	if (!connection) {
 		error = g_error_new_literal (NM_MANAGER_ERROR,
@@ -985,6 +986,20 @@ pending_activation_check_authorized (PendingActivation *pending,
 		                             "Connection could not be found.");
 		pending->callback (pending, error);
 		g_error_free (error);
+		return;
+	}
+
+	/* Ensure the subject has permissions for this connection */
+	if (!nm_auth_uid_in_acl (connection,
+	                         priv->session_monitor,
+	                         sender_uid,
+	                         &error_desc)) {
+		error = g_error_new_literal (NM_MANAGER_ERROR,
+		                             NM_MANAGER_ERROR_PERMISSION_DENIED,
+		                             error_desc);
+		pending->callback (pending, error);
+		g_error_free (error);
+		g_free (error_desc);
 		return;
 	}
 
@@ -1834,6 +1849,7 @@ device_auth_done_cb (NMAuthChain *chain,
 static void
 device_auth_request_cb (NMDevice *device,
                         DBusGMethodInvocation *context,
+                        NMConnection *connection,
                         const char *permission,
                         gboolean allow_interaction,
                         NMDeviceAuthRequestFunc callback,
@@ -1848,6 +1864,20 @@ device_auth_request_cb (NMDevice *device,
 
 	/* Get the caller's UID for the root check */
 	if (!nm_auth_get_caller_uid (context, priv->dbus_mgr, &sender_uid, &error_desc)) {
+		error = g_error_new_literal (NM_MANAGER_ERROR,
+		                             NM_MANAGER_ERROR_PERMISSION_DENIED,
+		                             error_desc);
+		callback (device, context, error, user_data);
+		g_error_free (error);
+		g_free (error_desc);
+		return;
+	}
+
+	/* Ensure the subject has permissions for this connection */
+	if (!nm_auth_uid_in_acl (connection,
+	                         priv->session_monitor,
+	                         sender_uid,
+	                         &error_desc)) {
 		error = g_error_new_literal (NM_MANAGER_ERROR,
 		                             NM_MANAGER_ERROR_PERMISSION_DENIED,
 		                             error_desc);
@@ -3324,6 +3354,20 @@ impl_manager_deactivate_connection (NMManager *self,
 		return;
 	}
 
+	/* Ensure the subject has permissions for this connection */
+	if (!nm_auth_uid_in_acl (connection,
+	                         priv->session_monitor,
+	                         sender_uid,
+	                         &error_desc)) {
+		error = g_error_new_literal (NM_MANAGER_ERROR,
+		                             NM_MANAGER_ERROR_PERMISSION_DENIED,
+		                             error_desc);
+		dbus_g_method_return_error (context, error);
+		g_error_free (error);
+		g_free (error_desc);
+		return;
+	}
+
 	/* Yay for root */
 	if (0 == sender_uid) {
 		if (!nm_manager_deactivate_connection (self,
@@ -4392,6 +4436,8 @@ nm_manager_new (NMSettings *settings,
 	                  G_CALLBACK (bluez_manager_bdaddr_removed_cb),
 	                  singleton);
 
+	priv->session_monitor = nm_session_monitor_get ();
+
 	/* Force kernel WiFi rfkill state to follow NM saved wifi state in case
 	 * the BIOS doesn't save rfkill state, and to be consistent with user
 	 * changes to the WirelessEnabled property which toggles kernel rfkill.
@@ -4461,6 +4507,7 @@ dispose (GObject *object)
 
 	g_object_unref (priv->settings);
 	g_object_unref (priv->vpn_manager);
+	g_object_unref (priv->session_monitor);
 
 	if (priv->modem_added_id) {
 		g_source_remove (priv->modem_added_id);

src/nm-device.c
@@ -3861,6 +3861,7 @@ disconnect_cb (NMDevice *device,
 static void
 impl_device_disconnect (NMDevice *device, DBusGMethodInvocation *context)
 {
+	NMConnection *connection;
 	GError *error = NULL;
 
 	if (NM_DEVICE_GET_PRIVATE (device)->act_request == NULL) {
@@ -3872,9 +3873,13 @@ impl_device_disconnect (NMDevice *device, DBusGMethodInvocation *context)
 		return;
 	}
 
+	connection = nm_device_get_connection (device);
+	g_assert (connection);
+
 	/* Ask the manager to authenticate this request for us */
 	g_signal_emit (device, signals[AUTH_REQUEST], 0,
 	               context,
+	               connection,
 	               NM_AUTH_PERMISSION_NETWORK_CONTROL,
 	               TRUE,
 	               disconnect_cb,
@@ -4839,9 +4844,9 @@ nm_device_class_init (NMDeviceClass *klass)
 		              G_OBJECT_CLASS_TYPE (object_class),
 		              G_SIGNAL_RUN_FIRST,
 		              0, NULL, NULL,
-		              /* dbus-glib context, permission, allow_interaction, callback, user_data */
-		              _nm_marshal_VOID__POINTER_STRING_BOOLEAN_POINTER_POINTER,
-		              G_TYPE_NONE, 5, G_TYPE_POINTER, G_TYPE_STRING, G_TYPE_BOOLEAN, G_TYPE_POINTER, G_TYPE_POINTER);
+		              /* dbus-glib context, connection, permission, allow_interaction, callback, user_data */
+		              _nm_marshal_VOID__POINTER_POINTER_STRING_BOOLEAN_POINTER_POINTER,
+		              G_TYPE_NONE, 6, G_TYPE_POINTER, G_TYPE_POINTER, G_TYPE_STRING, G_TYPE_BOOLEAN, G_TYPE_POINTER, G_TYPE_POINTER);
 
 	signals[IP4_CONFIG_CHANGED] =
 		g_signal_new (NM_DEVICE_IP4_CONFIG_CHANGED,

commit 4099c9242b091604466dda081dc5f6909354258b

core: fix crash during Wi-Fi rescan by emitting NM_DEVICE_AUTH_REQUEST signal correctly
Fixes regression introduced by commit da59b6d2229b85afbb9e14846931376340dc6030.

https://mail.gnome.org/archives/networkmanager-list/2014-May/msg00038.html

Backport-of: d9653484859c426f707b4759bc9882f7a57c1152
[thaller@redhat.com: original patch modified in nm-device-wifi.c to pass no
                     connection, as it is done on master]
Signed-off-by: Thomas Haller <thaller@redhat.com>

src/nm-device-wifi.c
@@ -1563,6 +1563,7 @@ impl_device_request_scan (NMDeviceWifi *self,
 	g_signal_emit_by_name (device,
 	                       NM_DEVICE_AUTH_REQUEST,
 	                       context,
+	                       NULL,
 	                       NM_AUTH_PERMISSION_NETWORK_CONTROL,
 	                       TRUE,
 	                       request_scan_cb,

src/nm-manager.c
@@ -1874,10 +1874,10 @@ device_auth_request_cb (NMDevice *device,
 	}
 
 	/* Ensure the subject has permissions for this connection */
-	if (!nm_auth_uid_in_acl (connection,
-	                         priv->session_monitor,
-	                         sender_uid,
-	                         &error_desc)) {
+	if (connection != NULL && !nm_auth_uid_in_acl (connection,
+	                                               priv->session_monitor,
+	                                               sender_uid,
+	                                               &error_desc)) {
 		error = g_error_new_literal (NM_MANAGER_ERROR,
 		                             NM_MANAGER_ERROR_PERMISSION_DENIED,
 		                             error_desc);