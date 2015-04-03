zyhuf/NetworkManager

commit 600489003ff2eca33865a3d41abcc9fc03fd6bb1

device: set the reason for when deactivating for another activation
It's always user requested -- auto activation never happens on already active
devices. nm_device_release_one_slave() rightly asserts teardown with
(un)configuration does not happen for no reason at all.

https://bugzilla.gnome.org/show_bug.cgi?id=744812

src/devices/nm-device.c
@@ -5834,7 +5834,7 @@ nm_device_queue_activation (NMDevice *self, NMActRequest *req)
 		_LOGI (LOGD_DEVICE, "disconnecting for new activation request.");
 		nm_device_state_changed (self,
 		                         NM_DEVICE_STATE_DEACTIVATING,
-		                         NM_DEVICE_STATE_REASON_NONE);
+		                         NM_DEVICE_STATE_REASON_USER_REQUESTED);
 	}
 }
 

commit bd7b3190624a74be1c4ab4fb1e6a1609796a0708

device: allow slave release with NONE reason
It happens when a device is disconnected for a new activation.

Fixes: 600489003ff2eca33865a3d41abcc9fc03fd6bb1

src/devices/nm-device.c
@@ -1002,9 +1002,6 @@ nm_device_release_one_slave (NMDevice *self, NMDevice *slave, gboolean configure
 	if (!configure) {
 		g_warn_if_fail (reason == NM_DEVICE_STATE_REASON_NONE || reason == NM_DEVICE_STATE_REASON_REMOVED);
 		reason = NM_DEVICE_STATE_REASON_NONE;
-	} else if (reason == NM_DEVICE_STATE_REASON_NONE) {
-		g_warn_if_reached ();
-		reason = NM_DEVICE_STATE_REASON_UNKNOWN;
 	}
 	nm_device_slave_notify_release (info->slave, reason);
 
@@ -1703,8 +1700,7 @@ nm_device_slave_notify_release (NMDevice *self, NMDeviceStateReason reason)
 	NMDeviceState new_state;
 	const char *master_status;
 
-	if (   reason != NM_DEVICE_STATE_REASON_NONE
-	    && priv->state > NM_DEVICE_STATE_DISCONNECTED
+	if (   priv->state > NM_DEVICE_STATE_DISCONNECTED
 	    && priv->state <= NM_DEVICE_STATE_ACTIVATED) {
 		if (reason == NM_DEVICE_STATE_REASON_DEPENDENCY_FAILED) {
 			new_state = NM_DEVICE_STATE_FAILED;