zyhuf/NetworkManager

commit adbf383628138b4f868456996123c4900537a4fb

device: when setting back a device to managed, update internal sys state
Otherwise a device which was set as unmanaged (updated to the REMOVED
internal sys-state) will never update its own sys-state if later set
back as managed.
Manage either when setting explictly the device to managed either when
just upping a connection on an unmanaged device.

src/nm-manager.c
@@ -3138,6 +3138,8 @@ unmanaged_to_disconnected (NMDevice *device)
 	 * and force the device to be managed. */
 	nm_device_set_unmanaged_by_flags (device, NM_UNMANAGED_PLATFORM_INIT, FALSE, NM_DEVICE_STATE_REASON_USER_REQUESTED);
 
+	if (nm_device_sys_iface_state_get (device)  == NM_DEVICE_SYS_IFACE_STATE_REMOVED)
+		nm_device_sys_iface_state_set (device, NM_DEVICE_SYS_IFACE_STATE_ASSUME);
 	nm_device_set_unmanaged_by_flags (device, NM_UNMANAGED_USER_EXPLICIT, FALSE, NM_DEVICE_STATE_REASON_USER_REQUESTED);
 
 	g_return_if_fail (nm_device_get_managed (device, FALSE));

src/devices/nm-device.c
@@ -13858,9 +13858,14 @@ set_property (GObject *object, guint prop_id,
 			NMDeviceStateReason reason;
 
 			managed = g_value_get_boolean (value);
-			if (managed)
+			if (managed) {
 				reason = NM_DEVICE_STATE_REASON_CONNECTION_ASSUMED;
-			else {
+				if (NM_IN_SET_TYPED (NMDeviceSysIfaceState,
+				                     priv->sys_iface_state,
+				                     NM_DEVICE_SYS_IFACE_STATE_EXTERNAL,
+				                     NM_DEVICE_SYS_IFACE_STATE_REMOVED))
+					nm_device_sys_iface_state_set (self, NM_DEVICE_SYS_IFACE_STATE_ASSUME);
+			} else {
 				reason = NM_DEVICE_STATE_REASON_REMOVED;
 				nm_device_sys_iface_state_set (self, NM_DEVICE_SYS_IFACE_STATE_REMOVED);
 			}

commit 7a8660a750edf9632b2fe924b15be867a02c2b23

manager: when a connection is upped on a device, do an early update of its internal state
When a user forces up a connection on a device, mark earlier the
device as managed: this would allow proper clean-up on the device also
when it was previously unmanaged or assumed.
This would avoid skipping IPv6LL address generation when instead it was
needed.

Fixes: adbf383628138b4f868456996123c4900537a4fb

https://bugzilla.redhat.com/show_bug.cgi?id=1452046

src/nm-manager.c
@@ -3178,8 +3178,6 @@ unmanaged_to_disconnected (NMDevice *device)
 	 * and force the device to be managed. */
 	nm_device_set_unmanaged_by_flags (device, NM_UNMANAGED_PLATFORM_INIT, FALSE, NM_DEVICE_STATE_REASON_USER_REQUESTED);
 
-	if (nm_device_sys_iface_state_get (device)  == NM_DEVICE_SYS_IFACE_STATE_REMOVED)
-		nm_device_sys_iface_state_set (device, NM_DEVICE_SYS_IFACE_STATE_ASSUME);
 	nm_device_set_unmanaged_by_flags (device, NM_UNMANAGED_USER_EXPLICIT, FALSE, NM_DEVICE_STATE_REASON_USER_REQUESTED);
 
 	g_return_if_fail (nm_device_get_managed (device, FALSE));
@@ -3543,6 +3541,9 @@ _new_active_connection (NMManager *self,
 		                                   subject,
 		                                   error);
 	}
+
+	if (device && (activation_type == NM_ACTIVATION_TYPE_MANAGED))
+		nm_device_sys_iface_state_set (device, NM_DEVICE_SYS_IFACE_STATE_MANAGED);
 
 	return (NMActiveConnection *) nm_act_request_new (settings_connection,
 	                                                  applied,