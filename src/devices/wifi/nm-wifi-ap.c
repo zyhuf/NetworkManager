b434521d68c959e64faf9467c38f413e8a9d99db
Andy Kling <andreas.kling@peiker-cee.de>
2019-02-20 10:08:27 +0100

    wifi/ap: detect mesh mode
    mark ap if supplicant reports bss property "Mode = 'mesh'".
    bss mode mesh is available since hostap_2_6-729-g213eb1885
    
    check mesh connections are compatible with detected mode.

--- src/devices/wifi/nm-wifi-ap.c
+++ src/devices/wifi/nm-wifi-ap.c
@@ -815,6 +815,8 @@ nm_wifi_ap_update_from_properties (NMWifiAP *ap,
 			changed |= nm_wifi_ap_set_mode (ap, NM_802_11_MODE_INFRA);
 		else if (!g_strcmp0 (s, "ad-hoc"))
 			changed |= nm_wifi_ap_set_mode (ap, NM_802_11_MODE_ADHOC);
+		else if (!g_strcmp0 (s, "mesh"))
+			changed |= nm_wifi_ap_set_mode (ap, NM_802_11_MODE_MESH);
 	}
 
 	if (g_variant_lookup (properties, "Signal", "n", &i16))
@@ -1007,7 +1009,9 @@ nm_wifi_ap_to_string (const NMWifiAP *self,
 	                        ? '#'
 	                        : (priv->fake
 	                               ? 'f'
-	                               : 'a'))),
+	                               : (priv->mode == NM_802_11_MODE_MESH
+	                                      ? 'm'
+	                                      : 'a')))),
 	            chan,
 	            priv->strength,
 	            priv->flags & NM_802_11_AP_FLAGS_PRIVACY ? 'P' : '_',
@@ -1071,6 +1075,8 @@ nm_wifi_ap_check_compatible (NMWifiAP *self,
 			return FALSE;
 		if (   !strcmp (mode, "ap")
 		    && (priv->mode != NM_802_11_MODE_INFRA || priv->hotspot != TRUE))
+			return FALSE;
+		if (!strcmp (mode, "mesh") && (priv->mode != NM_802_11_MODE_MESH))
 			return FALSE;
 	}
 
